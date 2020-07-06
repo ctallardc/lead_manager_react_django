72a2baac5dc2d68323a8065d2d8bc4616fd01c92
carlos tallard <ctallardc@yahoo.com>
2020-07-05 21:53:05 -0400

    First complete set of action in Redux get leads and deletelead

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -102,6 +102,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
